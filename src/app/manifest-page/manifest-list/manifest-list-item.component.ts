b86d15e7e60d7dc4f20a292e2096737764be12f7
Timon Davis <timondavis@yahoo.com>
2016-09-24 21:59:13 -0700

    [+] Layout of manifest tables are starting to shape up

--- src/app/manifest-page/manifest-list/manifest-list-item.component.ts
+++ src/app/manifest-page/manifest-list/manifest-list-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'spcc-manifest-list-item',
+  selector: '[spcc-manifest-list-item]',
   templateUrl: './manifest-list-item.component.html',
   styleUrls: ['./manifest-list-item.component.css']
 })
